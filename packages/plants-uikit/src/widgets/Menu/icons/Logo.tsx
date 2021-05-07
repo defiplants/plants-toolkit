c28bdaf77426735f19503792d802e23fd0c3eaea
Karl Mark Balagtey <karl.balagtey@gmail.com>
2021-05-07 19:10:29 +0100

    Updated logo

--- packages/plants-uikit/src/widgets/Menu/icons/Logo.tsx
+++ packages/plants-uikit/src/widgets/Menu/icons/Logo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { textColor } from "styled-system";
 
 interface LogoProps {
   isDark: boolean;
@@ -9,13 +10,14 @@ const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
     <LogoText style={{ color: `${textColor}`}} {...props}>
-      DigiPlants
+      DefiPlants
     </LogoText>
   );
 };
 
-const LogoText = styled.a`
+const LogoText = styled.p`
   font-weight: 600;
+  font-size: 24px;
 `;
 
 export default React.memo(Logo, (prev, next) => prev.isDark === next.isDark);
